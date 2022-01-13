6df59bc7c4ab9b9eb7ad89ebb134dcef2f483c55
rwclutterbuck <rwclutterbuck@outlook.com>
2022-01-13 11:51:40 +0000

    change to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+
+const colors = require('tailwindcss/colors')
+
+// module.exports = {
+// 	plugins: [
+// 		require('@themesberg/flowbite/plugin')
+// 	]
+// }
+module.exports = {
+    theme: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        gray: colors.gray,
+        emerald: colors.emerald,
+        indigo: colors.indigo,
+        yellow: colors.yellow,
+      },
+    },
+  }
